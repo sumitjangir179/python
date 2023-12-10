10264094a433f52c7ea3f61d15ecad8120d911d0
sumit jangir <sumitjangir179@gmail.com>
2023-12-10 21:23:54 +0530

    single inheritence

---
+++ single_inheritence.py
@@ -0,0 +1,38 @@
+class Employee:
+    def __init__(self,name,company,age,mobileNo,address):
+        self.name = name
+        self.company = company
+        self.age = age
+        self.mobileNo = mobileNo
+        self.address = address
+
+    def showDetails(self):
+        print(f'Name {self.name}')
+        print(f'mobile Number {self.mobileNo}')
+        print(f'Age {self.age}')
+        print(f'Address {self.address}')
+
+class Programmer(Employee):
+    def __init__(self, name, company, age, mobileNo, address,salary,level):
+        super().__init__(name, company, age, mobileNo, address)
+        self.salary = salary
+        self.level = level
+        self.role = 'Programmer'
+
+    
+    def showDetails(self):
+        print(f'Name {self.name}')
+        print(f'mobile Number {self.mobileNo}')
+        print(f'Age {self.age}')
+        print(f'Address {self.address}')
+        print(f'Salary {self.salary}')
+        print(f'Level {self.level}')
+        print(f'Role {self.role}')
+    
+
+if __name__ == '__main__':
+    # stark = Employee("stark",'google',22,'9828xxxx', 'India')
+    stark = Programmer("stark",'google',22,'9828xxxx', 'India',"5LPA","Senior")
+    stark.showDetails()
+
+# Note Local class method take precendence 
